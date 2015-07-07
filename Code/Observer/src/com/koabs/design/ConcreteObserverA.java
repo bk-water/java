29b0d889df3b1ce6466990950b7bbb43b6f329f3
白开水 <kevin1@kevindeMacBook-Pro.local>
2015-07-08 00:33:00 +0800

    Observer Pattern
    观察者模式

---
+++ Code/Observer/src/com/koabs/design/ConcreteObserverA.java
@@ -0,0 +1,11 @@
+package com.koabs.design;
+
+/**
+ * Created by kevin1 on 7/6/15.
+ */
+public class ConcreteObserverA extends Observer{
+    @Override
+    public void Update() {
+       System.out.println("Observer update.");
+    }
+}
---
+++ Code/Observer/src/com/koabs/design/ConcreteSubjectA.java
@@ -0,0 +1,8 @@
+package com.koabs.design;
+
+/**
+ * Created by kevin1 on 7/6/15.
+ */
+public class ConcreteSubjectA extends Subject{
+
+}
