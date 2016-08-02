67918ffcef858b4da02ed05d3a7f2cdf32b88b6d
pengpeng.cheng <pengpeng.cheng@china.zhaogang.com>
2016-08-02 11:56:48 +0800

    初始化

--- Code/Observer/src/com/koabs/design/ConcreteSubjectA.java
+++
@@ -1,8 +0,0 @@
-package com.koabs.design;
-
-/**
- * Created by kevin1 on 7/6/15.
- */
-public class ConcreteSubjectA extends Subject{
-
-}
--- Code/Observer/src/com/koabs/design/Observer.java
+++
@@ -1,9 +0,0 @@
-package com.koabs.design;
-
-/**
- * Created by kevin1 on 6/26/15.
- */
-public abstract class Observer {
-    // Methods
-    abstract public void Update();
-}
