ad9b128eb9aee071906a27bbebc0e87b926b93d1
koabs <koabs@foxmail.com>
2019-05-17 18:01:02 +0800

    ...

---
+++ basic-info/core/src/main/java/com/koabs/spi/OptimusPrime.java
@@ -0,0 +1,13 @@
+package com.koabs.spi;
+
+/**
+ * @Author koabs
+ * @Date 2019/5/14.
+ * @Describe
+ */
+public class OptimusPrime implements Robot {
+    @Override
+    public void sayHello() {
+        System.out.println("Hello, I am Optimus Prime.");
+    }
+}
